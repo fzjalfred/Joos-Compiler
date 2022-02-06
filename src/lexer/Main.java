b3c7d94dfd6d65160e22c5ef748087fa68f3d362
9Tempest <z277zhu@uwaterloo.ca>
2022-02-06 10:57:20 -0500

    [zhenyan] fix bug filename match class name

--- src/lexer/Main.java
+++ src/lexer/Main.java
@@ -6,10 +6,26 @@ import java_cup.runtime.*;
 
 public class Main {
 
+	static private void checkFileName(String thisFileName,String publicFilename, List<String> classList) throws Exception{
+		String baseName = thisFileName.split(".+?/(?=[^/]+$)")[1].split("\\.(?=[^\\.]+$)")[0];
+		// check public file name matches thisFileName or not
+		if (!publicFilename.equals(baseName) && publicFilename != ""){
+			throw new Exception(  "class: " + publicFilename + " does not match " + baseName);
+		}
+		for (String o : classList){
+			if (baseName.equals(o)) {
+				return;
+			}
+		}
+		throw new Exception(  "no class name match " + baseName);
+	}
+
 	static public void main(String argv[]) {
 		try {
-			parser p = new parser(new Lexer(new FileReader(argv[0])));
+			String fileName = argv[0];
+			parser p = new parser(new Lexer(new FileReader(fileName)));
 			Symbol result = p.debug_parse();
+			checkFileName(fileName,p.publicFileName, p.classList);
 			System.out.println(result.value);
 			System.out.println("success");
 		} catch (Exception e) {
