433575628e8e01f6ed139f321e3706ce4a74e489
richellali <richella@v1040-wn-rt-b-108-121.campus-dynamic.uwaterloo.ca>
2022-03-26 14:57:47 -0400

    add testcase

--- test/self_test_a4/foo.java
+++ test/self_test_a4/foo.java
@@ -2,15 +2,23 @@ public class foo{
     public foo(){
 
     }
+    public int bar(int a) {
+        return a;
+    }
 
     public int main(){
         int a = 10+10*5;
         int b = 20;
+        int r = bar(a);
         boolean bool = false;
         boolean bool2 = true;
         {
             int c = 10 -5;
             int d = 10 / 5;
+        }
+        if (a == 3) {
+            a = 4;
+            b = 5;
         }
         return a;
     }
