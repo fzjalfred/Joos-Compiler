2876cfdd69e138ae982adfc38fc8907d5220da48
z277zhu <z277zhu@uwaterloo.ca>
2022-04-14 10:58:08 -0400

    [zhenyan] add run assembly bash script
    Signed-off-by: z277zhu <z277zhu@uwaterloo.ca>

---
+++ run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+./joosc $1 $(find stdlib/2.0/java/ -name *.java )
+cd output
+/u/cs444/bin/nasm -O1 -f elf -g -F dwarf foo.java.s
+/u/cs444/bin/nasm -O1 -f elf -g -F dwarf Object.java.s
+/u/cs444/bin/nasm -O1 -f elf -g -F dwarf runtime.s
+ld -melf_i386 -o main *.o
