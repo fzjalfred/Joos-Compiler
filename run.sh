4c5fd99a8431eb42e07b9b19c9ecdb883e57a009
j2333li <j2333li@uwaterloo.ca>
2022-04-15 10:42:30 -0400

    add bash script for testing

--- run.sh
+++ run.sh
@@ -1,12 +1,23 @@
 #!/bin/bash
-rm output/*.o
-./joosc $1 $2 $(find stdlib/2.0/java/ -name *.java )
+FILES=($(ls output/*.java.s))
+for file in "${FILES[@]}"
+do
+  rm $file
+done
+FILES=($(ls output/*.java.o))
+for file in "${FILES[@]}"
+do
+  rm $file
+done
+
+./joosc $* $(find stdlib/2.0/java/ -name *.java )
 cd output
-/u/cs444/bin/nasm -O1 -f elf -g -F dwarf classTestA.java.s
-/u/cs444/bin/nasm -O1 -f elf -g -F dwarf classTest.java.s
-/u/cs444/bin/nasm -O1 -f elf -g -F dwarf Object.java.s
-/u/cs444/bin/nasm -O1 -f elf -g -F dwarf runtime.s
+FILES=($(ls *.s))
+
+for file in "${FILES[@]}"
+do
+  /u/cs444/bin/nasm -O1 -f elf -g -F dwarf $file
+  done
 ld -melf_i386 -o main *.o
 ./main
 echo $?
-
