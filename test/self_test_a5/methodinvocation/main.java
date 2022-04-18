c1038733184f6915a6699891ebe4ff3ea0b56523
fzj <z63feng@uwaterloo.ca>
2022-04-17 22:16:10 -0400

    cannot find entry symbol _start
    Signed-off-by: fzj <z63feng@uwaterloo.ca>

--- test/self_test_a5/methodinvocation/main.java
+++ test/self_test_a5/methodinvocation/main.java
@@ -1,8 +1,10 @@
 public class main {
-    public main() {
+    public main() {}
+    public static int test() {
         // interface1 testin1 = null; // not passed
         interface1 testin1 = new A();
         //testin1.myA()e;
         testin1.setX();
+        return 52;
     }
 }
