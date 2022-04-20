ac5d80b87c72ffe4e815422d2b6852604904f376
z277zhu <z277zhu@uwaterloo.ca>
2022-04-20 13:48:30 -0400

    [zhenyan] add stdlib
    Signed-off-by: z277zhu <z277zhu@uwaterloo.ca>

--- src/backend/IRTranslator.java
+++ src/backend/IRTranslator.java
@@ -27,7 +27,7 @@ public class IRTranslator {
 
     public void translate(){
         for (CompilationUnit comp : comps){
-            if (!comp.fileName.contains("stdlib") || comp.fileName.contains("Object") || comp.fileName.contains("String")){
+            if (!comp.fileName.contains("stdlib") || comp.fileName.contains("Object") || comp.fileName.contains("String") || comp.fileName.contains("Arrays") ){
                 System.out.println(comp.fileName);
                 comp.accept(visitor);
                 ir_comps.add(visitor.compUnit);
